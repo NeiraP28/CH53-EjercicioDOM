d70018c0bc4c9971fe56d3761ffbdcdd8bcfd642
Neira P <neyra140719@gmail.com>
2025-04-08 10:15:20 -0600

    Se agregan los archivos de inicio del proyecto

---
+++ script.js
@@ -0,0 +1,40 @@
+
+//doument es el objeto que nos permite acceder a todos el cuerpo del DOM
+
+
+let btnMostrar = document.getElementById("btnMostrar");
+let encabezado1 = document.getElementById("encabezado1");
+let encabezado2 = document.getElementById("encabezado2");
+let listas = document.getElementsByTagName("ul");
+
+let elementos = document.getElementsByClassName("list-group-item");
+
+let otroElemento = document.querySelector("ul>li"); //trae el primero que encuentra
+
+let otrosElementos = document.querySelectorAll("ul>li"); //todos
+console.log(otrosElementos.length);
+console.log("otroElemento:" , otroElemento);
+
+console.log(listas.length);//2
+//console.log(listas[0]);
+console.log(listas.item(1));
+
+
+let contador = 0;
+function modifica (){
+encabezado1.innerHTML = "<em>Ejercicio</em> DOM"; //diferencia entre text y html, html 
+//hace cambios en el documento y no los muestra, text si. 
+encabezado2.innerText = "DOMM ejercicio";
+} //modifica
+
+/* console.log(encabezado1.innerText); //innerText me permite modificar la propiedad dentro del documento 
+console.log(encabezado2.innerText);
+ */
+
+
+btnMostrar.addEventListener("click", function(event){
+    event.preventDefault(); //no hagas lo que haces por defecto 
+    console.log("botón btnModificar presionado");
+    
+});
+
